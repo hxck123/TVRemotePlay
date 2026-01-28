0a0be53aed14c763d65011a932e58c9622a8ffac
hxck <70248545+hxck123@users.noreply.github.com>
2026-01-28 11:16:09 +0800

    Update IMediaDataSource.java

--- ijkplayer/src/main/java/tv/danmaku/ijk/media/player/misc/IMediaDataSource.java
+++ ijkplayer/src/main/java/tv/danmaku/ijk/media/player/misc/IMediaDataSource.java
@@ -1,1 +1,9 @@
+package tv.danmaku.ijk.media.player.misc;
 
+import java.io.IOException;
+
+public interface IMediaDataSource {
+    int readAt(long position, byte[] buffer, int offset, int size) throws IOException;
+    long getSize() throws IOException;
+    void close() throws IOException;
+}
