f41b5ad8642efdb3cc503ea0113b04d52618c967
hxck <70248545+hxck123@users.noreply.github.com>
2026-01-28 11:13:31 +0800

    Update IjkTimedText.java

--- ijkplayer/src/main/java/tv/danmaku/ijk/media/player/IjkTimedText.java
+++ ijkplayer/src/main/java/tv/danmaku/ijk/media/player/IjkTimedText.java
@@ -1,1 +1,21 @@
+package tv.danmaku.ijk.media.player;
 
+import android.graphics.Rect;
+
+public class IjkTimedText {
+    private Rect mTextBounds;
+    private String mTextChars;
+
+    public IjkTimedText(Rect bounds, String text) {
+        mTextBounds = bounds;
+        mTextChars = text;
+    }
+
+    public Rect getBounds() {
+        return mTextBounds;
+    }
+
+    public String getText() {
+        return mTextChars;
+    }
+}
