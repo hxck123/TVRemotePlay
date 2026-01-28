d7b389244b82e61294f15c10846f45cd286d647b
hxck <70248545+hxck123@users.noreply.github.com>
2026-01-28 11:19:29 +0800

    Update FileMediaDataSource.java

--- ijkplayer/src/main/java/player/widget/media/FileMediaDataSource.java
+++ ijkplayer/src/main/java/player/widget/media/FileMediaDataSource.java
@@ -1,57 +1,40 @@
-/*
- * Copyright (C) 2015 Bilibili
- * Copyright (C) 2015 Zhang Rui <bbcallen@gmail.com>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package player.widget.media;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.RandomAccessFile;
 
 import tv.danmaku.ijk.media.player.misc.IMediaDataSource;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class FileMediaDataSource implements IMediaDataSource {
-    private RandomAccessFile mFile;
-    private long mFileSize;
+    private FileInputStream mFileInputStream;
+    private File mFile;
 
     public FileMediaDataSource(File file) throws IOException {
-        mFile = new RandomAccessFile(file, "r");
-        mFileSize = mFile.length();
+        mFile = file;
+        mFileInputStream = new FileInputStream(file);
     }
 
     @Override
     public int readAt(long position, byte[] buffer, int offset, int size) throws IOException {
-        if (mFile.getFilePointer() != position)
-            mFile.seek(position);
-
-        if (size == 0)
-            return 0;
-
-        return mFile.read(buffer, 0, size);
+        if (position >= mFile.length()) {
+            return -1;
+        }
+        
+        mFileInputStream.getChannel().position(position);
+        return mFileInputStream.read(buffer, offset, size);
     }
 
     @Override
     public long getSize() throws IOException {
-        return mFileSize;
+        return mFile.length();
     }
 
     @Override
     public void close() throws IOException {
-        mFileSize = 0;
-        mFile.close();
-        mFile = null;
+        if (mFileInputStream != null) {
+            mFileInputStream.close();
+            mFileInputStream = null;
+        }
     }
 }
