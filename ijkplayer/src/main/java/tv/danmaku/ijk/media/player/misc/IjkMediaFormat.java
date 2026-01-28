1d2e9815a7d7adba95214acde0fbe45c8b725506
hxck <70248545+hxck123@users.noreply.github.com>
2026-01-28 11:16:42 +0800

    Update IjkMediaFormat.java

--- ijkplayer/src/main/java/tv/danmaku/ijk/media/player/misc/IjkMediaFormat.java
+++ ijkplayer/src/main/java/tv/danmaku/ijk/media/player/misc/IjkMediaFormat.java
@@ -1,1 +1,15 @@
+package tv.danmaku.ijk.media.player.misc;
 
+public class IjkMediaFormat {
+    public static final String KEY_IJK_CODEC_LONG_NAME_UI = "codec-long";
+    public static final String KEY_IJK_CODEC_PROFILE_LEVEL_UI = "profile-level";
+    public static final String KEY_IJK_CODEC_PIXEL_FORMAT_UI = "pixel-format";
+    public static final String KEY_IJK_RESOLUTION_UI = "resolution";
+    public static final String KEY_IJK_FRAME_RATE_UI = "frame-rate";
+    public static final String KEY_IJK_BIT_RATE_UI = "bit-rate";
+    public static final String KEY_IJK_SAMPLE_RATE_UI = "sample-rate";
+    public static final String KEY_IJK_CHANNEL_UI = "channel";
+    public static final String KEY_IJK_NB_STREAMS_UI = "nb-streams";
+    public static final String KEY_IJK_DURATION_UI = "duration";
+    public static final String KEY_IJK_START_TIME_UI = "start-time";
+}
