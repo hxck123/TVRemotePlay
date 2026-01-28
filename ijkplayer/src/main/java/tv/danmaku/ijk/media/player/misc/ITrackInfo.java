ad039d14c91f83207de095808cd8ce8258acd984
hxck <70248545+hxck123@users.noreply.github.com>
2026-01-28 11:17:06 +0800

    Update ITrackInfo.java

--- ijkplayer/src/main/java/tv/danmaku/ijk/media/player/misc/ITrackInfo.java
+++ ijkplayer/src/main/java/tv/danmaku/ijk/media/player/misc/ITrackInfo.java
@@ -1,1 +1,10 @@
+package tv.danmaku.ijk.media.player.misc;
 
+public interface ITrackInfo {
+    int MEDIA_TRACK_TYPE_AUDIO = 2;
+    int MEDIA_TRACK_TYPE_VIDEO = 1;
+    int MEDIA_TRACK_TYPE_SUBTITLE = 3;
+    int MEDIA_TRACK_TYPE_TIMEDTEXT = 4;
+    int MEDIA_TRACK_TYPE_METADATA = 5;
+    int MEDIA_TRACK_TYPE_UNKNOWN = 0;
+}
